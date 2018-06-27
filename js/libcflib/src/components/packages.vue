9e84607d565d78e72a6a75dd66e8200d3c5ae550
Anthony Scopatz <scopatz@gmail.com>
2018-06-27 08:59:56 -0400

    need to rethink REST API

--- js/libcflib/src/components/packages.vue
+++ js/libcflib/src/components/packages.vue
@@ -16,7 +16,11 @@
       <tr v-for="pkg in filteredPackages" v-bind:key="pkg.name + '5a10b298'">
         <td>{{ pkg.name }}</td>
         <td>{{ Object.keys(pkg.artifacts).join(', ') }}</td>
-        <td>{{ pkg.channel }}</td>
+        <!-- <td>{{ arches[pkg.name].join(', ') }}</td> -->
+        <!-- <td>{{ arches() }}</td> -->
+        <td v-for="arch in uniqueArches(pkg.name)" v-bind:key="arch + 'kjsjs'">{{ arch.keys()}} </td>
+        <!-- <td>{{ uniqueArches(pkg.name) }}</td> -->
+
       </tr>
     </tbody>
   </table>
@@ -32,7 +36,7 @@ export default {
     filterkey: "",
     order: 1,
     errors: [],
-    packages: {}
+    packages: {},
     }},
 
   // Fetches posts when the component is created.
@@ -52,7 +56,52 @@ export default {
       return Object.values(self.packages).filter(function(value) {
         return value.name.indexOf(self.filterkey) > -1;
         })
-    }
+    },
+  },
+
+  methods: {
+    arches() {
+      // Normalize arch data on fetch
+      var pkg;
+      var self = this;
+      var arcs = {};
+      var pkgArches = new Set([]);
+      if (Object.keys(self.packages).length === 0) {
+        return arcs;
+      }
+      for (pkg in Object.values(self.packages)) {
+        pkgArches = new Set([]);
+        var channel, a;
+        for (channel in Object.values(pkg.artifacts)) {
+          for (a in Object.keys(channel)) {
+            pkgArches.add(a);
+          }
+        }
+        arcs[pkg.name] = Array.from(pkgArches)
+      }
+      return arcs;
+    },
+    uniqueArches(name) {
+      // Normalize arch data on fetch
+      if (!(name in this.packages)) {
+        return [];
+      }
+      var pkg = this.packages[name];
+      //return pkg;
+      //return Object.values(pkg);
+      //return Object.values(pkg.artifacts);
+      var channels = Object.values(pkg.artifacts);
+      return channels;
+      var pkgArches = new Set([]);
+      for (var channel in channels) {
+      //for (var channel in Object.values(pkg.artifacts)) {
+        return [channel];
+        for (var arch in Object.keys(channel)) {
+          pkgArches.add(arch);
+        }
+      }
+      return Array.from(pkgArches);
+    },
   }
 }
 </script>
